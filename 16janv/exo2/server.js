9f563f730620db591b316fdfe9dd50fcbc0b806b
DWS-paris <j.noyer@digitalworkshop.fr>
2020-01-16 15:41:18 +0100

    Route POST  api/:endpoint

--- 16janv/exo2/server.js
+++ 16janv/exo2/server.js
@@ -4,8 +4,11 @@ Import
     // Nodes
     const exprress = require('express');
     const bodyParser = require('body-parser');
-    const ejs = require('ejs');
+    const ejs = require('ejs'); 
     const path = require('path');
+
+    // Modules
+    const fetch = require('node-fetch');
 //
 
 /* 
@@ -29,9 +32,62 @@ Config
             server.set( 'views', __dirname + '/www' );
             server.use( exprress.static(path.join( __dirname, 'www' )) );
 
+            // Get routes
+            this.serverRoutes();
+        };
+
+        serverRoutes(){
+            /* 
+            API route definition; CRUD
+            */
+                // CRUD: Create item
+                server.post( '/api/:endpoint', ( req, res ) => {
+                    const apiEndpoint = req.params['endpoint'];
+
+                    // Create new DB entry
+                    return new Promise( (resolve, reject) => {
+
+                        // Check body content
+                        //=> IF OK
+                        fetch( `http://localhost:3000/${apiEndpoint}`, {
+                            method: 'POST',
+                            body: JSON.stringify(req.body),
+                            headers: { 'Content-Type': 'application/json' }
+                        })
+                        .then( data => {
+                            // Resolve Promise  data
+                            return resolve( res.json({
+                                status: 201,
+                                msg: 'Item created',
+                                data: data,
+                                error: null
+                            }))
+                        })
+                        .catch( err => {
+                            // Reject Promise error
+                            return reject( res.json({
+                                status: 500,
+                                msg: 'Item not created',
+                                data: null,
+                                error: err
+                            }))
+                        })
+                    });
+                })
+
+                // CRUD: Read one item
+
+                // CRUD: Read all items
+
+                // CRUD: Update item by ID
+
+                // CRUD: Delete item by ID
+            //
+
+
             // Start server
             this.launch();
-        };
+        }
 
         launch(){
             server.listen( port, () => {
