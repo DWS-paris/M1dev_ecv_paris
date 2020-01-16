bb344386e71cd824dbcf6580c0c928c4588c8f19
DWS-paris <j.noyer@digitalworkshop.fr>
2020-01-16 12:03:17 +0100

    Ajout du popin

--- 16janv/js/main.js
+++ 16janv/js/main.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const urlInstagram = `https://www.instagram.com/explore/tags`;
         const mainTag = document.querySelector('main');
         const tagList = document.createElement('ul');
+        const popin = document.querySelector('#popin');
     //
 
     /* 
@@ -29,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .then( jsonData => {
                 displayTagList(jsonData.graphql.hashtag.edge_hashtag_to_media.edges);
+                
             })
             .catch( fetchError => {
                 console.error(fetchError);
@@ -55,8 +57,41 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             // Add UL tag in MAIN tag
-            mainTag.appendChild(tagList)
+            mainTag.appendChild(tagList);
+
+            // Get click on instagram list link
+            displayInstagramImage();
         }
+
+        // Display Instagram image in a popin
+        const displayInstagramImage = () => {
+            // Get all links in instagramTag
+            const instagramLinks = document.querySelectorAll('.instagramTag a');
+
+            // Get all clic on instagramLinks
+            for( let item of instagramLinks ){
+                // Get click event
+                item.addEventListener('click', event => {
+                    // Stop event propagation 
+                    event.preventDefault();
+
+                    // Get HREF attribute value
+                    let imageUrl = item.getAttribute('href');
+                    console.log('click', {imageUrl})
+
+                    // Display image in #popin
+                    popin.innerHTML = `<img src="${imageUrl}" alt="Photo Instagram">`;
+
+                    // Open popin
+                    popin.classList.add('open');
+
+                    // Close popin
+                    popin.addEventListener('click', () => {
+                        popin.classList.remove('open');
+                    })
+                });
+            };
+        };
     //
 
     /* 
